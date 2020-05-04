12111215189a3dbdbb690330715ba08dfd4deb81
Stijn Haulotte <stijn.haulotte@supremia.com>
2020-05-04 12:57:58 +0200

    Filtering on asset and marketing type

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Asset;
 use App\Entity\AssetType;
 use App\Entity\MarketingType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +13,10 @@ class ListController extends AbstractController
 {
     /**
      * @Route("/list", name="list")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $assetTypes = $this->getDoctrine()
             ->getRepository(AssetType::class)
@@ -21,9 +25,57 @@ class ListController extends AbstractController
             ->getRepository(MarketingType::class)
             ->findAll();
 
+        $dql = "SELECT
+                    DISTINCT location,
+                    id,
+                    game_id
+                from asset
+                    RIGHT JOIN asset_asset_type on asset.id=asset_asset_type.asset_id
+                    RIGHT JOIN asset_marketing_type on asset.id=asset_marketing_type.asset_id
+                WHERE (1 =1)";
+
+        $marketingType = ($request->query->get('marketing_type'));
+        if ($marketingType !== null) {
+            $marketingTypeArr = explode(',', $marketingType);
+            $marketingTypeArr = array_keys(array_intersect(array_count_values($marketingTypeArr),[1]));
+            $marketingType = implode($marketingTypeArr, ',');
+            if ($marketingType !== '') {
+                // @todo sql injection
+                $dql .= " AND asset_marketing_type.marketing_type_id in (" . ltrim($marketingType, ',') . ")";
+            }
+        }
+
+        $assetType = ($request->query->get('asset_type'));
+        if ($assetType !== null) {
+            $assetTypeArr = explode(',', $assetType);
+            $assetTypeArr = array_keys(array_intersect(array_count_values($assetTypeArr),[1]));
+            $assetType = implode($assetTypeArr, ',');
+            if ($assetType !== '') {
+                // @todo sql injection
+                $dql .= " AND asset_asset_type.asset_type_id in (" . ltrim($assetType, ',') . ")";
+            }
+        }
+
+        $dql .= "ORDER BY asset.id DESC";
+        $em = $this->getDoctrine()->getManager();
+        $statement = $em->getConnection()->prepare($dql);
+        $statement->execute();
+
+        $assetsObj = $statement->fetchAll();
+        $assets = array();
+        foreach ($assetsObj as $asset) {
+            $assetObj = $this->getDoctrine()
+                ->getRepository(Asset::class)
+                ->findOneBy(array('id' => $asset['id']));
+            $assets[] = $assetObj;
+        }
+
         return $this->render('list/index.html.twig', [
             'assetTypes' => $assetTypes,
             'marketingTypes' => $marketingTypes,
+            'assetType' => $assetType,
+            'marketingType' => $marketingType,
+            'assets' => $assets,
         ]);
     }
 }
